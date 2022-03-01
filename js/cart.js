897e3b413f7a244608af00dbde98b69c62044f2a
董聿凌 <gb2013429@gmail.com>
2022-03-01 23:39:44 +0800

    cart

---
+++ js/cart.js
@@ -0,0 +1,15 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
+
+//燈入及登入狀態、取得產品列表
+const site='https://vue3-course-api.hexschool.io/v2';
+const api_path='yuling202202';
+
+const app = createApp({
+    data(){
+        return{
+            
+        }
+    }
+});
+
+app.mount('#app');
